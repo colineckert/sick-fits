13e86847a1952e28ef8d2fcd8bb67c269b6e80da
colineckert <colineckert10@gmail.com>
2021-05-07 14:07:36 -0700

    Create page files

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <p>order page</p>
+    </div>
+  );
+}
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SellPage() {
+  return (
+    <div>
+      <p>sell page</p>
+    </div>
+  );
+}
