9ea2a7e9d23685a04cd32a37dfc5b5433c0e63cf
colineckert <colineckert10@gmail.com>
2021-05-10 13:26:59 -0700

    style single product component

--- sick-fits/frontend/components/SingleProduct.js
+++ sick-fits/frontend/components/SingleProduct.js
@@ -1,6 +1,22 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
 import DisplayError from './ErrorMessage';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--max-width);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -8,6 +24,13 @@ const SINGLE_ITEM_QUERY = gql`
       name
       price
       description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
     }
   }
 `;
@@ -19,9 +42,21 @@ export default function SingleProduct({ id }) {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
+  const { Product } = data;
+  console.log(Product);
   return (
-    <div>
-      <h2>{data.Product.name}</h2>
-    </div>
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
   );
 }
