72270238518c84bc662fafe577def2b48d1ca815
colineckert <colineckert10@gmail.com>
2021-06-16 09:26:49 -0700

    Create AddToCart component

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -34,10 +34,12 @@ function CartItem({ cartItem }) {
       />
       <div>
         <h3>{product.name}</h3>
-        <p>{formatMoney(product.price * cartItem.quantity)}</p>-
-        <em>
-          {cartItem.quantity} &times; {formatMoney(product.price)} each
-        </em>
+        <p>
+          {formatMoney(product.price * cartItem.quantity)} -
+          <em>
+            {cartItem.quantity} &times; {formatMoney(product.price)} each
+          </em>
+        </p>
       </div>
     </CartItemStyles>
   );
