10384b3e2adc621c306562b4e896b62d12b0c7f5
colineckert <colineckert10@gmail.com>
2021-06-22 16:53:16 -0700

    Create checkout component

---
+++ sick-fits/frontend/components/Checkout.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const CheckoutFormStyles = styled.form`
+  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
+  border: 1px solid rgba(0, 0, 0, 0.06);
+  border-radius: 5px;
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+export default function Checkout() {
+  return (
+    <CheckoutFormStyles>
+      <p>Hey</p>
+    </CheckoutFormStyles>
+  );
+}
