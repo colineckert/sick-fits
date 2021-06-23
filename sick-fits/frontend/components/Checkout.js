01ee3a5e38ace7d6da364f3944d34d0c0d2bfcff
colineckert <colineckert10@gmail.com>
2021-06-22 17:05:41 -0700

    Fix error in product pathname

--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -9,10 +11,14 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
 export default function Checkout() {
   return (
-    <CheckoutFormStyles>
-      <p>Hey</p>
-    </CheckoutFormStyles>
+    <Elements stripe={stripeLib}>
+      <CheckoutFormStyles>
+        <p>Hey</p>
+      </CheckoutFormStyles>
+    </Elements>
   );
 }
